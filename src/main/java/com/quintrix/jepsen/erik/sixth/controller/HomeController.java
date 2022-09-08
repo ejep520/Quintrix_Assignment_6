b86c243dd9f81ec350cf3d9d0b126d90b7b03138
Erik Jepsen <5465915+ejep520@users.noreply.github.com>
2022-09-08 13:14:25 -0700

    Replaced properties file with YAML and...
    Moved the post/patch headers into the class as a field, since they get recreated/reused often.
    Added delete- and patch-using methods.

--- src/main/java/com/quintrix/jepsen/erik/sixth/controller/HomeController.java
+++ src/main/java/com/quintrix/jepsen/erik/sixth/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.quintrix.jepsen.erik.sixth.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -9,8 +10,10 @@ import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 import com.quintrix.jepsen.erik.sixth.model.Person;
 
@@ -19,20 +22,25 @@ import com.quintrix.jepsen.erik.sixth.model.Person;
                 // definition, making subsequent @ResponseBody annotations within the
                 // @RestController redundant.
 public class HomeController {
-  private SimpleClientHttpRequestFactory simpleClientHttpRequestFactory;
+  private HttpHeaders httpPostHeaders;
   private RestTemplate restTemplate;
+  private SimpleClientHttpRequestFactory simpleClientHttpRequestFactory;
+
+  @Value("${sixth.baseUri}")
+  private String baseUri;
 
   public HomeController() {
     simpleClientHttpRequestFactory = new SimpleClientHttpRequestFactory();
     simpleClientHttpRequestFactory.setReadTimeout(5000);
     simpleClientHttpRequestFactory.setConnectTimeout(5000);
     restTemplate = new RestTemplate(simpleClientHttpRequestFactory);
+    httpPostHeaders = new HttpHeaders();
+    httpPostHeaders.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
   }
 
   @GetMapping("/robert/find")
-  public String home() {
-    Person[] people =
-        restTemplate.getForObject("http://localhost:8080/person/first/robert", Person[].class);
+  public String robertFind() {
+    Person[] people = restTemplate.getForObject(baseUri + "/person/first/robert", Person[].class);
     String reply = "";
     for (int i = 0; i < people.length; i++) {
       reply += people[i].toString();
@@ -44,17 +52,14 @@ public class HomeController {
 
   @GetMapping("/robert/add")
   public ResponseEntity<String> robertAdd() {
-    HttpHeaders headers = new HttpHeaders();
-    headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-
     MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
     map.add("fName", "Robert");
     map.add("lName", "Johnson\", -1); DROP TABLE persons;");
     map.add("deptId", "3");
 
-    HttpEntity<MultiValueMap<String, String>> httpEntity = new HttpEntity<>(map, headers);
+    HttpEntity<MultiValueMap<String, String>> httpEntity = new HttpEntity<>(map, httpPostHeaders);
     ResponseEntity<String> response =
-        restTemplate.postForEntity("http://localhost:8080/person/new", httpEntity, String.class);
+        restTemplate.postForEntity(baseUri + "/person/new", httpEntity, String.class);
     if (response.getBody().equals("Success"))
       return response;
     return new ResponseEntity<String>("Fail", HttpStatus.NOT_ACCEPTABLE);
@@ -62,17 +67,13 @@ public class HomeController {
 
   @GetMapping("/robert/fail")
   public ResponseEntity<String> robertFail() {
-    HttpHeaders headers = new HttpHeaders();
-    headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-
     MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
     map.add("fName", "Robert");
 
-    HttpEntity<MultiValueMap<String, String>> httpEntity = new HttpEntity<>(map, headers);
+    HttpEntity<MultiValueMap<String, String>> httpEntity = new HttpEntity<>(map, httpPostHeaders);
     ResponseEntity<String> response;
     try {
-      response =
-          restTemplate.postForEntity("http://localhost:8080/person/new", httpEntity, String.class);
+      response = restTemplate.postForEntity(baseUri + "/person/new", httpEntity, String.class);
     } catch (HttpClientErrorException e) {
       String reply = e.getLocalizedMessage() + System.lineSeparator();
       for (StackTraceElement element : e.getStackTrace()) {
@@ -84,4 +85,27 @@ public class HomeController {
     return response;
   }
 
+  @GetMapping("/robert/delete/{id}")
+  public void robertDelete(@PathVariable int id) {
+    try {
+      restTemplate.delete(baseUri + "/person/delete/{id}", id);
+    } catch (RestClientException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @GetMapping("/robert/rename/{id}/{lName}")
+  public String robertRename(@PathVariable Integer id, @PathVariable String lName) {
+    MultiValueMap<String, String> map;
+    HttpEntity<MultiValueMap<String, String>> requestEntity;
+    String response;
+
+    map = new LinkedMultiValueMap<String, String>();
+    map.add("lName", lName);
+
+    requestEntity = new HttpEntity<>(map, httpPostHeaders);
+
+    return restTemplate.patchForObject(baseUri + "/person/" + id.toString() + "/last",
+        requestEntity, String.class);
+  }
 }
